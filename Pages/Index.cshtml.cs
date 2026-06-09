54d4b9d432ec5f16ed18fdb921c3c65f9051b0cd
HansKindberg <hans.kindberg@gmail.com>
2026-06-09 10:12:08 +0200

    Index.cshtml

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using EPiServer.Web.Mvc;
+using OptimizelyCms13Lab.Models.Pages;
+
+namespace OptimizelyCms13Lab.Pages;
+
+public class IndexModel : RazorPageModel<StartPage>
+{
+    public void OnGet()
+    {
+    }
+}
