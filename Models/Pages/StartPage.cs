70d584ade7492e1ad8470a8026894765723f169a
HansKindberg <hans.kindberg@gmail.com>
2026-06-09 10:05:22 +0200

    StartPage.cs

---
+++ Models/Pages/StartPage.cs
@@ -0,0 +1,7 @@
+﻿namespace OptimizelyCms13Lab.Models.Pages;
+
+[ContentType(GUID = "e0c3635e-d978-4f2b-bb9c-e6e525bb57d5")]
+public class StartPage : PageData
+{
+    public virtual string? Test { get; set; }
+}
